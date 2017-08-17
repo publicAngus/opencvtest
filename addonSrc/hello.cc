31921a0482639f04f6ba603ff74d184dd1c8337f
angus <angus@virtualBox.ubuntu14-04>
2017-08-18 01:12:20 +0800

    single folder library compile success

--- addonSrc/hello.cc
+++ addonSrc/hello.cc
@@ -1,4 +1,6 @@
 #include <node.h>
+#include <iostream>
+#include <string>
 
 namespace demo {
 using v8::FunctionCallbackInfo;
@@ -7,14 +9,67 @@ using v8::Local;
 using v8::Object;
 using v8::String;
 using v8::Value;
+using v8::Function;
+using v8::Number;
+using v8::FunctionTemplate;
+using namespace std;
 
 void Method(const FunctionCallbackInfo<Value>& args){
+
+    int arr[2]{8,19};
+	std::string str= "hello 熱門";
+	for(int i : arr){
+		str+= std::to_string(i);
+	}
+
+	string test;
+	//cin >> test;
+
+	str += test;
+   
+    //str = args[0]->ToString();
+	//std::cout << str << "sldjfls" << std::endl;
+
 	Isolate* isolate = args.GetIsolate();
-	args.GetReturnValue().Set(String::NewFromUtf8(isolate,"world"));
+	args.GetReturnValue().Set(String::NewFromUtf8(isolate, str.c_str()));
 }
+
+void ReturnObjectVal(const FunctionCallbackInfo<Value>& args){
+	Isolate* isolate = args.GetIsolate();
+	Local<Object> obj = Object::New(isolate);
+    
+	obj->Set(String::NewFromUtf8(isolate,"im obj"),args[0]->ToString());
+	args.GetReturnValue().Set(obj);
+
+}
+
+void RunJsCallback(const FunctionCallbackInfo<Value>& args){
+	Isolate* isolate = args.GetIsolate();
+	Local<Function> cb = Local<Function>::Cast(args[0]);
+	const unsigned int ind =2;
+	Local<Value> argv[ind] = {String::NewFromUtf8(isolate,"arg1"),Number::New(isolate,100)};
+	cb->Call(Null(isolate),ind,argv);
+}
+
+void FunctionReturn(const FunctionCallbackInfo<Value>& args){
+	std::cout<<"function return called" << std::endl;
+	Isolate* isolate = args.GetIsolate();
+	args.GetReturnValue().Set(9001);
+}
+
+void ReturnFunctionToJs(const FunctionCallbackInfo<Value>& args){
+	Isolate* isolate = args.GetIsolate();
+	Local<FunctionTemplate> funcTmpl= FunctionTemplate::New(isolate,FunctionReturn);
+	Local<Function> func = funcTmpl->GetFunction();  
+	args.GetReturnValue().Set(func);
+}
+
 
 void init(Local<Object> exports){
 	NODE_SET_METHOD(exports,"hello",Method);
+	NODE_SET_METHOD(exports,"getObj",ReturnObjectVal);
+	NODE_SET_METHOD(exports,"callmeBack",RunJsCallback);
+	NODE_SET_METHOD(exports,"getJsFunc",ReturnFunctionToJs);
 }
 
 NODE_MODULE(addon,init)
---
+++ addonSrc/src/ns_test/tester2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+
+using namespace std;
+int main(int i,char* args[]){
+    std::cout<< "helloworld";
+    std::cout<< std::endl;
+    
+    string str = "im heloo 2";
+
+    std::cout << str;
+    std::cout<< std::endl;
+
+    std::getchar();
+}
