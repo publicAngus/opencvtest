952164d2f334b4251a4cf4e3d6201dae611d13ef
angus <angus@virtualBox.ubuntu14-04>
2018-01-02 22:47:46 +0800

    binary tree drawn

--- addonSrc/main.cpp
+++ addonSrc/main.cpp
@@ -19,25 +19,29 @@ using namespace cv;
 
 #define macroTest(p)(p*p*99)
 
-void drawNode(cv::Point ptn,int levelCount,BiNodeDraw bnd, BiNode<int> bnode){
+void drawNode(cv::Point ptn,int counter,BiNodeDraw bnd, BiNode<int> *bnode){
         cv::Point *nodePt;
         nodePt = &ptn;
-        int counter = levelCount;
         int nodeInterval;
-        int powerIndex;
-        for(int i=0;i<counter;i++){
-            powerIndex = (counter -i);
-            
-            nodeInterval = 3*pow(2,powerIndex - 3) -1;
-            if(nodeInterval <= 1) nodeInterval = 1;
-            
-            if(i != counter -1){
-                    bnd.DrawNode(*nodePt,nodeInterval,&bnode, i == counter -2?0:100);
+        int powerIndex = counter;
+        nodeInterval = 3*pow(2,powerIndex - 3) -1;
+        if(counter == 2) nodeInterval = 1;
+        bnd.DrawNode(*nodePt,nodeInterval,bnode,30);
+        
+        if(counter > 2){
+            if(bnode->RcPoint){
+                BiNode<int> *nBn = new BiNode<int>(counter-1);
+                nBn->InsertLC(counter+1000);
+                nBn->InsertRC(counter+2000);
+                drawNode(*(bnode->RcPoint),counter-1,bnd,nBn);
             }
-            nodePt  = bnode.RcPoint;
 
-            if((counter - 1) >= 2)
-            drawNode(*bnode.LcPoint,counter -1,bnd,bnode);
+            if(bnode->LcPoint){
+                BiNode<int> *nBnL = new BiNode<int>(counter-1);
+                nBnL->InsertLC(counter+3000);
+                nBnL->InsertRC(counter+4000);
+                drawNode(*(bnode->LcPoint),counter-1,bnd,nBnL);
+            }
 
         }
 }
@@ -45,8 +49,8 @@ void drawNode(cv::Point ptn,int levelCount,BiNodeDraw bnd, BiNode<int> bnode){
 int main(int i,char* args[]){
    
 
-   int labelWidth = 20;
-   int labelHeight = 20;
+   int labelWidth = 3;
+   int labelHeight = 3;
    BiNodeDraw bnd("helloworld",2000,1000,labelWidth,labelHeight);
      BiNode<int> bnode(101);
      bnode.InsertLC(200);
@@ -54,28 +58,10 @@ int main(int i,char* args[]){
      
    cv::Point ptn(800,0);
    
-   drawNode(ptn,5,bnd,bnode);
+   drawNode(ptn,8,bnd,&bnode);
+   waitKey(0);
 
-   /*
-   cv::Point *nodePt;
-   nodePt = &ptn;
-   int counter = 2;
-   int nodeInterval = 1;
-   int nextNodeInterval = 0;
-   int powerIndex = 0;
-   for(int i=0;i<counter;i++){
-       powerIndex = (counter -i);
-       
-        nodeInterval = 3*pow(2,powerIndex - 3) -1;
-        if(nodeInterval <= 1) nodeInterval = 1;
-       
-       std::cout << "nodeInterval:" << nodeInterval << std::endl;
-       if(i != counter -1){
-            bnd.DrawNode(*nodePt,nodeInterval,&bnode, i == counter -2?0:100);
-       }
-       nodePt  = bnode.RcPoint;
-   }
-    */
+   
    bnd.ReleaseWindow();
   
 
